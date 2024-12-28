d943f49f80750345ed3665de61d5d80ce331375d
wqwwaaaa <131134853+wqwwaaaa@users.noreply.github.com>
2024-12-28 11:18:01 +0300

    Add files via upload

---
+++ les14/src/zad1/Practicum.java
@@ -0,0 +1,19 @@
+package zad1;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Practicum {
+    private static Map<String, String> films = new LinkedHashMap<>();
+
+    public static void main(String[] args) {
+        films.put("Титаник", "Джеймс Кэмерон");
+        films.put("Гарри Поттер и философский камень", "Крис Коламбус");
+        films.put("Хакеры", "Иэн Софтли");
+        films.put("Терминатор", "Джеймс Кэмерон");
+
+        for (String film : films.keySet()) {
+            System.out.println(film);
+        }
+    }
+}
